c6ae97d7b9e27b380010c50e9341c0f76dec7a4d
Fabian Leyrer <fabianleyrer677@gmail.com>
2024-03-13 11:25:43 +0100

    Kalender für das erste Fenster ergänzt.

--- MaxTemp/MainWindow.xaml.cs
+++ MaxTemp/MainWindow.xaml.cs
@@ -3,22 +3,12 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MaxTemp
 {
-
-
     public partial class MainWindow : Window
     {
         public bool isDarkMode = false;
@@ -52,9 +42,129 @@ namespace MaxTemp
             changeMode(isDarkMode);
         }
 
+        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                DateTime selectedDate = calendar.SelectedDate ?? DateTime.Now;
 
-        private void changeMode(bool isDarkMode) 
+                // Führe die Berechnungen für das ausgewählte Datum durch
+                CalculateStatisticsForDate(selectedDate);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fehler beim Auswerten: {ex.Message}");
+            }
+        }
 
+        private void CalculateStatisticsForDate(DateTime selectedDate)
+        {
+            try
+            {
+                string filePath = "temps.csv";
+                double highestTemperature = double.MinValue;
+                double lowestTemperature = double.MaxValue;
+                double totalTemperature = 0;
+                int temperatureCount = 0;
+                Dictionary<string, Dictionary<double, int>> sensorTemperatureFrequency = new Dictionary<string, Dictionary<double, int>>();
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        string[] values = line.Split(',');
+
+                        if (values.Length >= 3)
+                        {
+                            DateTime currentDate = DateTime.ParseExact(values[1], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+
+                            if (currentDate.Date == selectedDate.Date)
+                            {
+                                string sensorName = values[0];
+                                double temperature = double.Parse(values[2], CultureInfo.InvariantCulture);
+
+                                if (!sensorTemperatureFrequency.ContainsKey(sensorName))
+                                {
+                                    sensorTemperatureFrequency[sensorName] = new Dictionary<double, int>();
+                                }
+
+                                if (sensorTemperatureFrequency[sensorName].ContainsKey(temperature))
+                                {
+                                    sensorTemperatureFrequency[sensorName][temperature]++;
+                                }
+                                else
+                                {
+                                    sensorTemperatureFrequency[sensorName][temperature] = 1;
+                                }
+
+                                if (temperature > highestTemperature)
+                                {
+                                    highestTemperature = temperature;
+                                }
+
+                                if (temperature < lowestTemperature)
+                                {
+                                    lowestTemperature = temperature;
+                                }
+
+                                totalTemperature += temperature;
+                                temperatureCount++;
+                            }
+                        }
+                    }
+                }
+
+                string mostFrequentSensorHigh = "";
+                double highestTemperatureFrequency = 0;
+
+                foreach (var entry in sensorTemperatureFrequency)
+                {
+                    var maxFrequencyEntry = entry.Value.Aggregate((x, y) => x.Value > y.Value ? x : y);
+
+                    if (maxFrequencyEntry.Value > highestTemperatureFrequency)
+                    {
+                        highestTemperatureFrequency = maxFrequencyEntry.Value;
+                        mostFrequentSensorHigh = entry.Key;
+                    }
+                }
+
+                string mostFrequentSensorLow = "";
+                double lowestTemperatureFrequency = double.MaxValue;
+
+                foreach (var entry in sensorTemperatureFrequency)
+                {
+                    var minFrequencyEntry = entry.Value.Aggregate((x, y) => x.Value < y.Value ? x : y);
+
+                    if (minFrequencyEntry.Value < lowestTemperatureFrequency)
+                    {
+                        lowestTemperatureFrequency = minFrequencyEntry.Value;
+                        mostFrequentSensorLow = entry.Key;
+                    }
+                }
+
+                double averageTemperature = totalTemperature / temperatureCount;
+                UpdateUI(() =>
+                {
+                    txtHighestTemperature.Text = highestTemperature.ToString("F2", CultureInfo.InvariantCulture) + " C°";
+                    txtLowestTemperature.Text = lowestTemperature.ToString("F2", CultureInfo.InvariantCulture) + " C°";
+                    txtAverageTemperature.Text = averageTemperature.ToString("F2", CultureInfo.InvariantCulture) + " C°";
+                    txtMostFrequentSensorHigh.Text = $"{mostFrequentSensorHigh} - Häufigkeit: {highestTemperatureFrequency}";
+                    txtMostFrequentSensorLow.Text = $"{mostFrequentSensorLow} - Häufigkeit: {lowestTemperatureFrequency}";
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fehler beim Auswerten: {ex.Message}");
+            }
+        }
+
+        private void UpdateUI(Action action)
+        {
+            Dispatcher.BeginInvoke(action);
+        }
+
+        private void changeMode(bool isDarkMode)
         {
             isDarkMode = !isDarkMode;
             Brush foregroundColor = isDarkMode ? Brushes.White : Brushes.Black;
@@ -79,7 +189,6 @@ namespace MaxTemp
 
             txtMostFrequentSensorHigh.Foreground = foregroundColor;
             txtMostFrequentSensorLow.Foreground = foregroundColor;
-
         }
 
         private void ChangeAllTextForeground(DependencyObject parent, Brush brush)
